cf0874da0246e362ca10e3c7834c8793888c5818
Ahammed Shorif <ahammedshorif20@gmail.com>
2025-10-10 20:57:42 +0600

    font & url type change

--- token-launchpad/web3-token-launchpad/components/CreateToken.jsx
+++ token-launchpad/web3-token-launchpad/components/CreateToken.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 function CreateToken() {
   return (
-    <div className='h-screen w-full flex justify-center items-center bg-neutral-800'>
+    <div className='h-screen w-full flex justify-center items-center bg-neutral-800 font-serif'>
         <div className='text-white bg-neutral-950 w-[40%] h-[400px] p-3 rounded-lg'>
             <div className='flex flex-col gap-6 m-6'>
               <input className='p-2 border rounded-lg' type="text" placeholder='Token Name' />
               <input className='p-2 border rounded-lg' type="text" placeholder='Token Symbol' />
               <input className='p-2 border rounded-lg' type="text" placeholder='Initial Supply' />
-              <input className='p-2 border rounded-lg' type="text" placeholder='Token Image URL' />
+              <input className='p-2 border rounded-lg' type="url" placeholder='Token Image URL' />
               <button type="button" 
               className='mt-10 bg-amber-700 p-2 rounded-lg cursor-pointer'
               >Create Token</button>
